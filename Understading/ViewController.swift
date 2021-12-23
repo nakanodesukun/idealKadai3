e4b85013de990f0dbfb3dca8d025e98ffbffd580
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2021-12-23 15:32:05 +0900

    修正

--- Understading/ViewController.swift
+++ Understading/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
+    @IBOutlet private weak var firstLabel: UILabel!
+    @IBOutlet private weak var secondLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var leftSwitch: UISwitch!
+    @IBOutlet private weak var rightSwitch: UISwitch!
+    
+    
+    
+    @IBAction func resultButton(_ sender: Any) {
+        guard let num1 = firstTextField.text.flatMap({Double($0)}), let num2 = secondTextField.text.flatMap({Double($0)}) else { return resultLabel.text = "数値を入力して下さい"}
+//        三項演算子で判別
+        let leftNum = leftSwitch.isOn ? -num1: num2
+        let rightNum = rightSwitch.isOn ? -num1 : num2
+        
+        firstLabel.text = String(leftNum)
+        secondLabel.text = String(rightNum)
+        resultLabel.text = String(leftNum + rightNum)
     }
-
-
 }
-
